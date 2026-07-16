051f839fa656daabebede04231779c34585554d6
Michael Curtis <kingfishsolutions-ai@gmail.com>
2026-07-16 10:36:58 +1000

    Summary test 16 Jul
    Commit summary 2

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requestedddxxxxxx %s' % (self.path)
+        msg = 'Hello! you requestedddxxxxxxyyy %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
